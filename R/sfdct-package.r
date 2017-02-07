713160786a891dc2a70e5a43427ea92ce2b4ce0d
Michael Sumner <mdsumner@gmail.com>
2017-02-08 02:48:17 +1100

    doc example

--- R/sfdct-package.r
+++ R/sfdct-package.r
@@ -14,6 +14,9 @@ NULL
 #'
 #' See data-raw for original source.
 #' @name antarctica
+#' @examples
+#' library(sf)
+#' plot(antarctica, col = rainbow(nrow(antarctica), alpha = 0.4))
 NULL
 
 #' The 'world' database from the `maps` package.
